abdb482e2afeeea4eaae20f64826e6c050a668ba
JNeo77 <lifted_2000@yahoo.com>
2025-02-28 20:05:39 -0500

    Refactor Gameboard Constructor and Methods
    Use a property on Gameboard class with an array value to include all 5
    ships.
    
    Refactor methods with a parameter that takes a ship as an argument.

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,27 +4,29 @@ export class Gameboard {
   constructor() {
     this.board = Array(10).fill(null).map(() => Array(10).fill(null));
     this.misses = new Set();
+    this.allShips = [
+      { type: 'Carrier', shipObj: new Ship(5) },
+      { type: 'Battleship', shipObj: new Ship(4) },
+      { type: 'Cruise', shipObj: new Ship(3) },
+      { type: 'Submarine', shipObj: new Ship(3) },
+      { type: 'Destroyer', shipObj: new Ship(2) }
+    ];
     this.sunkenShips = [];
   }
 
-  placeShip(row, col, length) {
-    const ship = new Ship(length);
+  placeShip(row, col, ship) {
 
-    if (ship.direction === 'horizontal') {
-      for (let i = col - 1; i < col - 1 + ship.length; i++) {
-        if (this.board[row - 1][i] === null) { 
-          this.board[row - 1][i] = ship;
-        } else {
-          throw new Error ('Space already occupied by another ship!');
-        }
+    const { type, shipObj } = ship;
+    
+    this.checkIfOccupied(row, col, shipObj);
+
+    if (shipObj.direction === 'horizontal') {
+      for (let i = col - 1; i < col - 1 + shipObj.length; i++) {
+        this.board[row - 1][i] = shipObj;
       }
     } else {
-      for (let i = row - 1; i < row - 1 + ship.length; i++) {
-        if (this.board[i][col - 1] === null) { 
-          this.board[i][col - 1] = ship;
-        } else {
-          throw new Error ('Space already occupied by another ship!');
-        }
+      for (let i = row - 1; i < row - 1 + shipObj.length; i++) {
+        this.board[i][col - 1] = shipObj;
       }
     }
   }
@@ -41,5 +43,21 @@ export class Gameboard {
 
   allShipsSunk() {
     return this.sunkenShips.length === 5;
+  }
+
+  checkIfOccupied(row, col, ship) {
+    if (ship.direction === 'horizontal') {
+      for (let i = col - 1; i < col - 1 + ship.length; i++) {
+        if (this.board[row - 1][i] !== null) {
+          throw new Error ('Space already occupied by another ship!');
+        }
+      }
+    } else {
+      for (let i = row - 1; i < row - 1 + ship.length; i++) {
+        if (this.board[i][col - 1] !== null) { 
+          throw new Error ('Space already occupied by another ship!');
+        }
+      }
+    }
   }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -8,33 +8,41 @@ test("gameboard gets constructed", () => {
 
 test("gameboard places ships horizontally", () => {
   const gameBoard = new Gameboard;
-  gameBoard.placeShip(1, 1, 2);
+  const ship = gameBoard.allShips[4];
+  gameBoard.placeShip(1, 1, ship);
   expect(gameBoard.board[0][0].hits).toBe(0);
   expect(gameBoard.board[0][1].hits).toBe(0);
 });
 
 test.skip("gameboard place ships vertically", () => {
   const gameBoard = new Gameboard;
-  gameBoard.placeShip(7, 9, 2);
+  const ship = gameBoard.allShips[4];
+  gameBoard.placeShip(7, 9, ship);
   expect(gameBoard.board[6][8].hits).toBe(0);
   expect(gameBoard.board[7][8].hits).toBe(0);
 });
 
 test("error is thrown if gameboard space already contains a ship", () => {
   const gameBoard = new Gameboard;
+  const ship = gameBoard.allShips[4];
 
-  gameBoard.placeShip(7, 9, 2);
+  gameBoard.placeShip(7, 9, ship);
   
   function expectError() {
-    gameBoard.placeShip(7, 9, 2);
+    gameBoard.placeShip(7, 8, ship);
   }
   
   expect(expectError).toThrow('Space already occupied by another ship!');
+  expect(gameBoard.board[6][7]).toBe(null);
+  expect(gameBoard.board[6][8]).toBeInstanceOf(Ship);
+  expect(gameBoard.board[6][9]).toBeInstanceOf(Ship);
 });
 
 test("attack misses a ship", () => {
   const gameBoard = new Gameboard;
-  gameBoard.placeShip(1, 1, 2);
+  const ship = gameBoard.allShips[4];
+
+  gameBoard.placeShip(1, 1, ship);
   gameBoard.receiveAttack(2, 2);
   gameBoard.receiveAttack(4, 5);
   gameBoard.receiveAttack(0, 0);
@@ -50,7 +58,9 @@ test("attack misses a ship", () => {
 
 test("attack hits a ship", () => {
   const gameBoard = new Gameboard;
-  gameBoard.placeShip(1, 1, 2);
+  const ship = gameBoard.allShips[4];
+
+  gameBoard.placeShip(1, 1, ship);
   
   gameBoard.receiveAttack(0,0);
   gameBoard.receiveAttack(0,1);
@@ -62,8 +72,9 @@ test("attack hits a ship", () => {
 
 test("gameboard keeps track of sunken ships", () => {
   const gameBoard = new Gameboard;
+  const ship = gameBoard.allShips[4];
 
-  gameBoard.placeShip(1, 1, 2);
+  gameBoard.placeShip(1, 1, ship);
   
   gameBoard.receiveAttack(0,0);
   gameBoard.receiveAttack(0,1);
@@ -73,21 +84,33 @@ test("gameboard keeps track of sunken ships", () => {
 
 test("gameboard reports whether or not all ships have been sunk", () => {
   const gameBoard = new Gameboard;
+  const ship1 = gameBoard.allShips[0];
+  const ship2 = gameBoard.allShips[1];
+  const ship3 = gameBoard.allShips[2];
+  const ship4 = gameBoard.allShips[3];
+  const ship5 = gameBoard.allShips[4];
   
-  gameBoard.placeShip(1, 1, 2);
-  gameBoard.placeShip(2, 1, 2);
-  gameBoard.placeShip(3, 1, 2);
-  gameBoard.placeShip(4, 1, 2);
-  gameBoard.placeShip(5, 1, 2);
+  gameBoard.placeShip(1, 1, ship1);
+  gameBoard.placeShip(2, 1, ship2);
+  gameBoard.placeShip(3, 1, ship3);
+  gameBoard.placeShip(4, 1, ship4);
+  gameBoard.placeShip(5, 1, ship5);
 
   gameBoard.receiveAttack(0,0);
   gameBoard.receiveAttack(0,1);
+  gameBoard.receiveAttack(0,2);
+  gameBoard.receiveAttack(0,3);
+  gameBoard.receiveAttack(0,4);
   gameBoard.receiveAttack(1,0);
   gameBoard.receiveAttack(1,1);
+  gameBoard.receiveAttack(1,2);
+  gameBoard.receiveAttack(1,3);
   gameBoard.receiveAttack(2,0);
   gameBoard.receiveAttack(2,1);
+  gameBoard.receiveAttack(2,2);
   gameBoard.receiveAttack(3,0);
   gameBoard.receiveAttack(3,1);
+  gameBoard.receiveAttack(3,2);
   gameBoard.receiveAttack(4,0);
   gameBoard.receiveAttack(4,1);
 
